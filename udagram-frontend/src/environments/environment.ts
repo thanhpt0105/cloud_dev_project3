d3c99959d098902e191e3ec5d8f5a601c87b1f2b
thanhpt0105 <thanhpt0105@gmail.com>
2024-08-17 02:52:05 +0800

    Update frontend environment with expose reverse proxy

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a307d7e5a752940b5a4c995eb729395a-949310266.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
